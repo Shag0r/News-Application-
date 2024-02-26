716e00bcecaf351a2d761899d2fbf6a4b5e166f6
Shagor Kumar Das <101504353+Shag0r@users.noreply.github.com>
2024-02-26 15:27:41 +0600

    Add files via upload

---
+++ Data/ApplicationDbContext.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
+using NewsApplication.Models;
+
+namespace NewsApplication.Data
+{
+    public class ApplicationDbContext : IdentityDbContext
+    {
+        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options)
+            : base(options)
+        {
+        }
+        public DbSet<NewsApplication.Models.News> News { get; set; } = default!;
+    }
+}
---
+++ Models/News.cs
@@ -0,0 +1,13 @@
+﻿namespace NewsApplication.Models
+{
+    public class News
+    {
+        public int newsID { get; set; }
+        public string newsName { get; set;}
+        public string newsDescription { get; set;} = string.Empty;
+        public News()
+        {
+            
+        }
+    }
+}
